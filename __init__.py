b73a01b923f86191558694113322bdfb3684f12d
ImpactFrames <jvoxel@gmail.com>
2024-10-29 16:09:26 +0000

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -3,16 +3,13 @@ import importlib.util
 import glob
 import shutil
 from .IFHFDownloadNode import IFHFDownload 
-from .IFDisplayTextNode import IFDisplayText
 
 NODE_CLASS_MAPPINGS = {
     "IF_HFDownloadNode": IFHFDownload,
-    "IF_DisplayTextNode": IFDisplayText,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "IF_HFDownloadNode": "Hugging Face Download🤗",
-    "IF_DisplayTextNode": "IF Display Text📟",
 }
 
 WEB_DIRECTORY = "./web"
